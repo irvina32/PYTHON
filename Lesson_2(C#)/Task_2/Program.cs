25eaa2c59dc2c9792685aa05c4eb3a6610ee3d50
Irina <irvina@inbox.ru>
2022-10-20 16:32:43 +0300

    Задача_2 со вторым вариантом

--- Lesson_2(C#)/Task_2/Program.cs
+++ Lesson_2(C#)/Task_2/Program.cs
@@ -12,3 +12,9 @@ FirstN = number / 100;
 ThirdN = number % 10;
 
 Console.Write(FirstN * 10 + ThirdN);
+
+//int number = new Random().Next(100, 999);
+//int result = ((number / 10) * 10) + number % 10;
+// Console.WriteLine(number);
+// Console.WriteLine(result);
+//System.Console.WriteLine($"Was {number}, Now {result});  - "Was" + number + "..." + result
