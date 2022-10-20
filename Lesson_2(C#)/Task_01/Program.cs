6b274ca8db33ae8edd5efef49cd45f4894b15756
Irina <irvina@inbox.ru>
2022-10-20 15:34:36 +0300

    Задача_1

---
+++ Lesson_2(C#)/Task_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// прогр., кот.вывoдит случ. число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 99);
+
+System.Console.WriteLine(number);
+
+int firstNumber = 0;
+int secondNumder = 0;
+
+firstNumber = number/10;
+secondNumder = number % 10;
+
+if (firstNumber > secondNumder){
+        System.Console.WriteLine(firstNumber);
+} else {
+        System.Console.WriteLine(secondNumder);
+}
+
+
