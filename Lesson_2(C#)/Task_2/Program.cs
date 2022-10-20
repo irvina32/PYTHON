64bd7232df2f246d9102a391f32262d5ad59eea2
Irina <irvina@inbox.ru>
2022-10-20 16:10:26 +0300

    Задача_2

--- Lesson_2(C#)/Task_2/Program.cs
+++ Lesson_2(C#)/Task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// прогр., которая выводит случайцное трехзначное число и удаляет вторую цифру из него.
+
+int number = new Random().Next(100, 999);
+
+Console.WriteLine(number);
+
+int FirstN = 0;
+//int SecondN = 0;
+int ThirdN = 0;
+
+FirstN = number / 100;
+ThirdN = number % 10;
+
+Console.Write(FirstN * 10 + ThirdN);
