341b3904d14a43cda57fc9da4624df2b42dd4884
Irina <irvina@inbox.ru>
2022-10-21 11:39:16 +0300

    Задача_4

---
+++ Lesson_2(C#)/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// прогр., которая приним. число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("ВВедите число");
+
+int number1 = int.Parse(Console.ReadLine());
+
+if (number1 % 7 == 0 && (number1 % 23 == 0))
+{
+    Console.WriteLine("кратно 7 и 23"); 
+}
+ 
+        else
+{
+    Console.WriteLine("не кратно");
+}
