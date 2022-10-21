d8f85cf5c208130ae8c16ee135ba55f5dc3e41d7
Irina <irvina@inbox.ru>
2022-10-21 11:09:59 +0300

    задача 3 из семинара 2

---
+++ Lesson_2(C#)/Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// прогр. котор. приним. 2 числа и выводит. является ли 1 число кратно 2(/на 2)? если не кратно, 
+//то программа выводит остаток.
+
+Console.WriteLine("First Number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Second Number: ");
+int b = int.Parse(Console.ReadLine());
+int result = a % b;
+
+if (result == 0){
+    Console.WriteLine("успешно разделили a на  b");
+}
+
+   else
+    {
+       Console.WriteLine(result); 
+    }
