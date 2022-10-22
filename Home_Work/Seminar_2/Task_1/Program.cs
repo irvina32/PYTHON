e86a7dbb60e66d3d87936f89344f59488c8ba067
Irina <irvina@inbox.ru>
2022-10-22 13:26:59 +0300

    Задача_1 семинар 2

---
+++ Home_Work/Seminar_2/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(number);
+
+
+int firstNumber = 0;
+int secondNumder = 0;
+
+
+firstNumber = number / 10;
+
+Console.Write(firstNumber % 10);
