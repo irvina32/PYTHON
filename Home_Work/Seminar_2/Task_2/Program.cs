a261abdfd7ab527124a7fc40d7fe351eab5e2b4b
Irina <irvina@inbox.ru>
2022-10-23 22:24:54 +0300

    задача 2 с лекции 2

---
+++ Home_Work/Seminar_2/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+ Console.WriteLine("ВВедите число");
+int number = int.Parse(Console.ReadLine());
+int array = number;
+
+
+{
+    for (int i = 0; i <= number; i++)
+    {
+        int minPosition = i;
+       
+        if (minPosition < 0)
+            Console.Read();
+            Console.WriteLine("третьей цифры нет");
+            
+
+        if (minPosition >= 2)
+            Console.Read();
+            break;
+            Console.WriteLine(number);
+    }       
+}
