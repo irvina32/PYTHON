7321ac0cf329163a2216eb7af8b5f18460d667de
Irina <irvina@inbox.ru>
2022-11-05 14:42:02 +0300

    Доп. Семинар №5

--- Home_Work/Seminar_5/Tasks/Program.cs
+++ Home_Work/Seminar_5/Tasks/Program.cs
@@ -4,75 +4,75 @@
 
 
 
-// int[] CreateArray(int size, int Min, int max)
-//  { int[] array = new int[size];
-//  Random rnd = new Random();
-// for (int i = 0; i < size; i++)
-//  { array[i] = rnd.Next(Min, max);
-// }
-// return array;
-// }
+int[] CreateArray(int size, int Min, int max)
+ { int[] array = new int[size];
+ Random rnd = new Random();
+for (int i = 0; i < size; i++)
+ { array[i] = rnd.Next(Min, max);
+}
+return array;
+}
 
-// int EvenNumber(int[] array)
-// {
-//     int count = 0;
-//     for ( int i =0; i < array.Length; i++)
-//      {
-//         if (array[i] % 2 == 0) count++;
-//      }
-//      return count;
-// }
+int EvenNumber(int[] array)
+{
+    int count = 0;
+    for ( int i =0; i < array.Length; i++)
+     {
+        if (array[i] % 2 == 0) count++;
+     }
+     return count;
+}
 
-// string GetArray(int[] array)
-// {
-//     string count = string.Empty;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         count = count + $"{array[i]}, ";
-//     }
-//     return count;
-// }
-// int[] array  = CreateArray(10, 100, 1000);
+string GetArray(int[] array)
+{
+    string count = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        count = count + $"{array[i]}, ";
+    }
+    return count;
+}
+int[] array  = CreateArray(10, 100, 1000);
 
-// System.Console.WriteLine(GetArray(array));
-// System.Console.WriteLine(EvenNumber(array));
+System.Console.WriteLine(GetArray(array));
+System.Console.WriteLine(EvenNumber(array));
 
 
 
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 //Найдите сумму элементов, стоящих на нечётных позициях.
 
-// int[] CreateArray(int size, int Min, int max)
-//  { int[] array = new int[size];
-//  Random rnd = new Random();
-// for (int i = 0; i < array.Length; i++)
-//  { array[i] = rnd.Next(Min, max);
-// }
-// return array;
-// }
+int[] CreateArray(int size, int Min, int max)
+ { int[] array = new int[size];
+ Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+ { array[i] = rnd.Next(Min, max);
+}
+return array;
+}
 
-// int[] SumOddNumber(int[] array){
-// int sum = 0;
-// for (int i =0; i < array.Length; i++)
-// {
-//     if (array[i] % 2 != 0) sum = sum + array[i];
-//  }  
-//   return array;
-// }
+int[] SumOddNumber(int[] array){
+int sum = 0;
+for (int i =0; i < array.Length; i++)
+{
+    if (array[i] % 2 != 0) sum = sum + array[i];
+ }  
+  return array;
+}
 
-// string GetArray(int[] array)
-// {
-//     string sum = string.Empty;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         sum = sum + $"{array[i]}, ";
-//     }
-//     return sum;
-// }
-// int[] array  = CreateArray(4, 1, 10);
+string GetArray(int[] array)
+{
+    string sum = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = sum + $"{array[i]}, ";
+    }
+    return sum;
+}
+int[] array  = CreateArray(4, 1, 10);
 
 
-// 
+
 
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу 
@@ -87,10 +87,10 @@ for (int i = 0; i < array.Length; i++)
 return  array;
 }
 
-int DifElements(int[] array){
-//int dif = 0;
-int MinElement = array[0];
-int MaxElement = array[0];
+double DifElements(int[] array){
+
+double MinElement = array[0];
+double MaxElement = array[0];
  for (int i = 0; i < array.Length; i++)
  {
     if (array[i] < MinElement) 
@@ -101,7 +101,7 @@ int MaxElement = array[0];
     {
          MaxElement = array[i];}
     }
-   int dif = MaxElement - MinElement;
+   double dif = MaxElement - MinElement;
 
   return dif;  
 }
@@ -120,29 +120,3 @@ Console.WriteLine(GetArray(array));
 Console.WriteLine("Разница между минимальным и максимальным элементом массива = " + DifElements(array));
 
 
-
-// double[] arrayRealNumbers = new double[10];
-//   for (int i = 0; i < arrayRealNumbers.Length; i++ )
-//   {
-//     arrayRealNumbers[i] = new Random().Next(1, 10);
-//     Console.Write(arrayRealNumbers[i] + " ");
-//   }
-
-// double maxNumber = arrayRealNumbers[0];
-// double minNumber = arrayRealNumbers[0];
-
-//   for (int i = 1; i < arrayRealNumbers.Length; i++)
-//   {
-//     if (maxNumber < arrayRealNumbers[i])
-//     {
-//       maxNumber = arrayRealNumbers[i];
-//     }
-//         if (minNumber > arrayRealNumbers[i])
-//     {
-//       minNumber = arrayRealNumbers[i];
-//     }
-//   }
-
-//   double decision = maxNumber - minNumber;
-
-//   Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {decision}");
