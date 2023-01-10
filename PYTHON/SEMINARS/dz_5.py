9015bd9ae41cb09d7d15032e2b960a6a1f1bf074
irvina32 <115579967+irvina32@users.noreply.github.com>
2023-01-10 20:00:38 +0300

    Add files via upload

---
+++ PYTHON/SEMINARS/dz_5.py
@@ -0,0 +1,186 @@
+# 1. Создайте программу для игры с конфетами человек против человека.
+
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. 
+# За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно
+# взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+# a) Добавьте игру против бота
+
+# b) Подумайте как наделить бота ""интеллектом""
+
+
+from random import randint
+
+
+def input_dat(name):
+    x = int(
+        input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, value):
+    print(
+        f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+
+def bot_calc(value):
+    k = randint(1, 29)
+    while value-k <= 28 and value > 29:
+        k = randint(1, 29)
+    return k
+
+
+player1 = input("Введите имя первого игрока: ")
+player2 = "Bot"
+value = int(input("Введите количество конфет на столе: "))
+flag = randint(0, 2)  # флаг очередности
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0
+counter2 = 0
+
+while value > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        value -= k
+        flag = False
+        p_print(player1, k, counter1, value)
+    else:
+        k = bot_calc(value)
+        counter2 += k
+        value -= k
+        flag = True
+        p_print(player2, k, counter2, value)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
+
+# ____________________________________________________________
+
+from random import randint
+
+
+def input_dat(name):
+    x = int(
+        input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, value):
+    print(
+        f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+
+player1 = input("Введите имя первого игрока: ")
+player2 = "Bot"
+value = int(input("Введите количество конфет на столе: "))
+flag = randint(0, 2)  # флаг очередности
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0
+counter2 = 0
+
+while value > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        value -= k
+        flag = False
+        p_print(player1, k, counter1, value)
+    else:
+        k = randint(1, 29)
+        counter2 += k
+        value -= k
+        flag = True
+        p_print(player2, k, counter2, value)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
+
+# 2. Создайте программу для игры в ""Крестики-нолики"".
+
+maps = [1,2,3,
+        4,5,6,
+        7,8,9]
+ 
+victories = [[0,1,2],
+             [3,4,5],
+             [6,7,8],
+             [0,3,6],
+             [1,4,7],
+             [2,5,8],
+             [0,4,8],
+             [2,4,6]]
+ 
+def print_maps():
+    print(maps[0], end = " ")
+    print(maps[1], end = " ")
+    print(maps[2])
+ 
+    print(maps[3], end = " ")
+    print(maps[4], end = " ")
+    print(maps[5])
+ 
+    print(maps[6], end = " ")
+    print(maps[7], end = " ")
+    print(maps[8])    
+ 
+def step_maps(step,symbol):
+    ind = maps.index(step)
+    maps[ind] = symbol
+ 
+def get_result():
+    win = ""
+ 
+    for i in victories:
+        if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
+            win = "X"
+        if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
+            win = "O"   
+             
+    return win
+ 
+game_over = False
+player1 = True
+ 
+while game_over == False:
+ 
+    print_maps()
+ 
+    if player1 == True:
+        symbol = "X"
+        step = int(input("Игрок 1, ваш ход: "))
+    else:
+        symbol = "O"
+        step = int(input("Игрок 2, ваш ход: "))
+ 
+    step_maps(step,symbol) 
+    win = get_result() 
+    if win != "":
+        game_over = True
+    else:
+        game_over = False
+ 
+    player1 = not(player1)        
+         
+print_maps()
+print("Победил", win)
+
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
